d4ee4fd27dc81b24916a9c17cf22c8798be5b0d7
Luke Mattson <tuc38841@temple.edu>
2019-06-25 21:15:35 -0400

    Created a list of 'peers' to be charted with selected stock
    Signed-off-by: Luke Mattson <tuc38841@temple.edu>

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import requests
 import json
 from charts import get_chart_info
 
-base_url = "https://sandbox.iexapis.com/stable/stock/"
-token = "?token=Tpk_b0410fc3685c4561980063dfcb5279a7"
+base_url = "https://cloud.iexapis.com/stable/stock/"
+token = "?token=pk_44bd5242c4ab4595b33dafa82c61ba1c"
+#old_base_url = "https://sandbox.iexapis.com/stable/stock/"
+#old_token = "?token=Tpk_b0410fc3685c4561980063dfcb5279a7"
 
 class Stock_shot:
     def __init__(self, stock):
@@ -36,15 +38,19 @@ class Stock_shot:
         print(company_name, exchange, industry)
 
 def search_stock():
-    stock_string = input('Enter a stock ticker, or multiple tickers separated by a ",": ').upper()
-    stock_lst = stock_string.replace(" ", "").split(",")
+    stock_string = input("Enter a company's stock ticker (ex. apple = aapl): ").upper()
+    stock_lst = stock_string.replace(" ", "").split()
     print(stock_lst)
     for stock in stock_lst:
         single_stock = Stock_shot(stock)
         single_stock.get_quote(stock)
-        single_stock.get_peers(stock)
+        peers = single_stock.get_peers(stock)
         single_stock.get_company(stock)
-    get_chart_info(stock_lst)
+    peers = requests.get(base_url + stock_string + '/peers' + token)
+    peers_json = json.loads(peers.text)
+    stock_plus_peers = stock_lst + peers_json
+    print(stock_plus_peers)
+    get_chart_info(stock_plus_peers)
 
 
 search_stock()
